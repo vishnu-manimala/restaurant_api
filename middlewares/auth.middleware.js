6bb6e19041695b02bbbd3dde59632b0150650e2a
vishnuManimala <vishnumanimala8133@gmail.com>
2024-08-29 21:26:25 +0530

    auth middleware error correction

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -3,15 +3,17 @@ const jwtHelper = require('../utils/jwt.helper');
 const isAuthorized = async( req, res, next ) => {
 
     //Checks for token in headers.
-    const token = req.headers.authorization;
+    const tokenString = req.headers.authorization;
+    const token  = tokenString.split(" ")[1];
     if(!token) return res.status(400).json({ status:'error', message: "Token not provided" });
 
     //verify jwt token
     const decode = await jwtHelper.verifyToken(token);
     if(!decode) return res.status(401).json({status:"Unauthorized access", message:"Token expired"});
 
-    req.role = decode.role;// adding user role to req object;
-    req.userId = decode._id;// adding user id to req object;
+    req.role = decode.data.role;// adding user role to req object;
+    req.userId = decode.data._id;// adding user id to req object;
+    
     next();
 
 }
